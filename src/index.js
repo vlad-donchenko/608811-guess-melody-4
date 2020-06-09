11c6e21a95c4a40bc49f146115347533be6d4f03
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-06-10 00:19:48 +0300

    Create first screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
 
+const ERROR_COUNT = 3;
+
+const init = () => {
+  ReactDOM.render(<App errorsCount={ERROR_COUNT}/>, document.querySelector(`#root`));
+};
+
+init();
